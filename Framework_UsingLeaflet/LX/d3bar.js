baf6b6f8ebd317ed13fb5f9fb932811d4885699d
WesleyOMorrow <39685540+WesleyOMorrow@users.noreply.github.com>
2021-03-06 14:35:29 +0100

    Create d3bar.js

---
+++ Framework_UsingLeaflet/LX/d3bar.js
@@ -0,0 +1,46 @@
+var dataArray = [70, 57, 45, 40, 30, 20];
+
+var canvas = d3.select("#barchart")
+    .append("svg")
+    .attr("width", 900).attr("height", 500);
+
+var bars = canvas.selectAll("rect")
+    .data(dataArray)
+    .enter()
+    .append("rect")
+    .attr('class', 'base')
+    .attr("width", function (d) {
+        return d * 10;
+    })
+    .attr("height", 35)
+    .attr("y", function (d, i) {
+        return i * 70;
+    })
+    .attr("fill", "#4CAF50");
+
+
+
+
+/*
+canvas.append("text")
+    .text(function(d){
+        return d;
+    }).attr;
+*/
+
+var dataArray1 = [30, 25, 20, 15, 10, 5];
+
+var bars1 = canvas.selectAll("rect.addon")
+    .data(dataArray1)
+    .enter()
+    .append("rect")
+    .attr('class', 'addon')
+    .attr("width", function (d) {
+        return d * 10;
+    })
+    .attr("height", 25)
+    .attr("y", function (d, i) {
+        return (i * 70) + 5;
+    })
+    .attr("fill", "orange");
+    //.attr("fill-width", "black");
