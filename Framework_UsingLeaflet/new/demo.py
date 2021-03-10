50bbfdbc3b45951095b7e5f588b6f615b68a77dc
Ubuntu <danferwang1123@gmail.com>
2021-03-10 13:48:51 +0100

    flask demo

--- Framework_UsingLeaflet/new/demo.py
+++ Framework_UsingLeaflet/new/demo.py
@@ -1,15 +1,14 @@
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/_add_numbers')
 def add_numbers():
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
     return jsonify(result=a + b)
 
+
 @app.route('/')
 def index():
-    return render_template('new_index.html')
-
-
-app.run('localhost',port=12345, debug=True)
+    return render_template('index.html')
