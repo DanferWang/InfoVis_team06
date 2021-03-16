e93770c4e9faf9c0e4dfe0949e76189cefb9fd14
Ubuntu <danferwang1123@gmail.com>
2021-03-16 12:05:03 +0100

    set max&min zoom

--- Framework_UsingLeaflet/new/static/js/map.js
+++ Framework_UsingLeaflet/new/static/js/map.js
@@ -14,7 +14,8 @@ var mymap = L.map('mapid', mapOptions);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
+  maxZoom: 8,
+  minZoom:3,
   id: 'mapbox/light-v9',
   tileSize: 512,
   zoomOffset: -1,
