e3175b7a6cc134531797a0e0c7304d5158f7efde
Jaak Lin <linjaak@gmail.com>
2021-03-27 12:49:43 +0100

    bar popup

--- Framework_UsingLeaflet/new/static/js/d3bar.js
+++ Framework_UsingLeaflet/new/static/js/d3bar.js
@@ -1,4 +1,7 @@
-
+// ! Everything before the function "updatePlotBar" could be delete
+// since it would automaticly call the function initially
+// ===========================================================================
+// ===========================================================================
 var dataArray = [66.9, 59.7, 60, 60.6, 62.3, 63.3, 64.3, 61.5, 63.8, 60.3, 66.9, 60.3, 62.2, 61.2, 66.6, 57.4, 59.1, 59.9, 71.7, 62.9, 66.3, 62.2, 61.2, 62.5, 60.7, 59.5, 67.1, 60.9, 66.1, 59.3, 68.2];
 
 
@@ -6,6 +9,8 @@ var dataArray = [66.9, 59.7, 60, 60.6, 62.3, 63.3, 64.3, 61.5, 63.8, 60.3, 66.9,
 var margin = {top: 30, right: 30, bottom: 70, left: 65},
     width = 540 - margin.left - margin.right,
     height = 840 - margin.top - margin.bottom;
+// the div show the tooltip (popup during mouseover)
+var div = d3.select("#barchart").append("div").attr("class", "toolTip");
 
 // append the svg object to the body of the page
 var canvas = d3.select("#barchart")
@@ -77,29 +82,27 @@ var bars = canvas.selectAll("rect")
                 d > 50 ? '#edf8b1' :
                   '#ffffcc';})
     
-    .on('mouseover', function (d, i) {
-    d3.select(this).transition()
-       .duration('50')
-       .attr('opacity', '.85');
-    //Makes the new div appear on hover:
-    div.transition()
-       .duration(50)
-       .style("opacity", 1);
-     })
-    
-    .on('mouseout', function (d, i) {
-      d3.select(this).transition()
-           .duration('50')
-           .attr('opacity', '1');});
+    .on("mouseover", function(d, i) {
 
 
+        d3.select(this).attr("fill", "DarkOrange");
+    })
+
+    .on("mousemove", function(d, i) {
+
+    })
+    .on("mouseout", function(d) {
+    });
+// ===========================================================================
+// ===========================================================================
 function updatePlotBar(array) {
 
     var remove = d3.select("#barchart").select("svg").remove();
+    var remove1 = d3.select("#barchart").select("div").remove();
     var margin = {top: 30, right: 30, bottom: 70, left: 65},
     width = 540 - margin.left - margin.right,
     height = 840 - margin.top - margin.bottom;
-
+    var div = d3.select("#barchart").append("div").attr("class", "toolTip");
 // append the svg object to the body of the page
 var canvas = d3.select("#barchart")
   .append("svg")
@@ -113,19 +116,46 @@ var canvas = d3.select("#barchart")
 var xdata = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus','Czech', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany',
 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Norway', 'Poland',
 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switerland', 'UK'];
-c=array.map((e,i)=>{return [e,xdata[i]]})
+combin=array.map((e,i)=>{return [e,xdata[i]]})
 
-c.sort((x,y)=>{
+combin.sort((x,y)=>{
     if(x[0]<y[0]){return 1;}else{return -1}
      })
-b_new=c.map(e=>e[0])
-a_new=c.map(e=>e[1])
-a_new=a_new.reverse()
-console.log(a_new)
+// a_new to store country name
+// a_new reverse in order to plot
+// b_new to store score value
+b_new=combin.map(e=>e[0])
+a_new=combin.map(e=>e[1])
+tmp_a=combin.map(e=>e[1])
+// tmp_a use to show tooltip
+a_new.reverse()
+
+
+// ==== arrange data in new ds ====
+// so that we could use this in tooltip and even maybe connect with map by country name
+// c in it represent: country name
+// value represent: score
+var data_t = [{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},{c:"Austria",value:50},
+{c:"Austria",value:50},];
+// replace the correct country name and score in it
+for (var i = 0; i < tmp_a.length; i++) {
+data_t[i].c = tmp_a[i];
+data_t[i].value = b_new[i];
+}
+// ================================
+
+
+// =======draw the coordinate======
+// y 
 var xScale3 = d3.scaleBand()
 .domain(a_new)
 .range([ height, 0]);
-
+// x
 var xAxis3 = d3.axisLeft().scale(xScale3);
 canvas.append('g')
 .attr('trasform','translate(150,0)')
@@ -135,9 +165,7 @@ canvas.append('g')
 .attr('transform', 'rotate(25)');
 
 var xScale1 = d3.scaleLinear()
-
 .domain([0, 100])
-
 .range([0, width - 155]);
 
 var xAxis1 = d3.axisTop().scale(xScale1);
@@ -148,41 +176,58 @@ canvas.append('g')
 
 
 
-
 // jieshu
 var bars = canvas.selectAll("rect")
-    .data(b_new)
+    .data(data_t)
     .enter()
     .append("rect")
     .attr('class', 'base')
     .attr("width", function (d) {
-        return d * 3;
+        return d.value * 3;
     })
     .attr("height", 18)
     .attr("y", function (d, i) {
         return i * 24;
     })
-    .attr("fill", function(d){return d > 80 ? '#0c2c84' :
-      d > 75 ? '#225ea8' :
-        d > 70 ? '#1d91c0' :
-          d > 65 ? '#41b6c4' :
-            d > 60 ? '#7fcdbb' :
-              d > 55 ? '#c7e9b4' :
-                d > 50 ? '#edf8b1' :
+    .attr("fill", function(d){return d.value > 80 ? '#0c2c84' :
+      d.value > 75 ? '#225ea8' :
+        d.value > 70 ? '#1d91c0' :
+          d.value > 65 ? '#41b6c4' :
+            d.value > 60 ? '#7fcdbb' :
+              d.value > 55 ? '#c7e9b4' :
+                d.value > 50 ? '#edf8b1' :
                   '#ffffcc';})
+    .on("mouseover", function(d, i) {
+        var num = i+1;
+        div.style("left", d3.event.pageX+10+"px");
+        div.style("top", d3.event.pageY-25+"px");
+        div.style("display", "inline");
+        div.style("opacity", 1)
+        div.html("Rank: "+num+"<br>"+"Country: "+(d.c)+"<br>"+"Score: "+(d.value));
+        d3.select(this).attr("fill", "DarkOrange");
+        d3.select(this).attr("fill", "DarkOrange");
+    })
 
-    .on('mouseover', function (d, i) {
-    d3.select(this).transition()
-       .duration('50')
-       .attr('opacity', '.85');
-    //Makes the new div appear on hover:
-    div.transition()
-       .duration(50)
-       .style("opacity", 1);
-     })
-    
-    .on('mouseout', function (d, i) {
-      d3.select(this).transition()
-           .duration('50')
-           .attr('opacity', '1');});
+    .on("mousemove", function(d, i) {
+        var num = i+1;
+        div.style("left", d3.event.pageX+10+"px");
+        div.style("top", d3.event.pageY-25+"px");
+        div.style("display", "inline");
+        div.style("opacity", 1)
+        div.html("Rank: "+num+"<br>"+"Country: "+(d.c)+"<br>"+"Score: "+(d.value));
+        d3.select(this).attr("fill", "DarkOrange");
+    })
+    .on("mouseout", function(d) {
+        div.style("display", "none");
+        div.style("opacity", 0)
+        d3.select(this).attr("fill", function(d){return d.value > 80 ? '#0c2c84' :
+            d.value > 75 ? '#225ea8' :
+                d.value > 70 ? '#1d91c0' :
+                  d.value > 65 ? '#41b6c4' :
+                    d.value > 60 ? '#7fcdbb' :
+                      d.value > 55 ? '#c7e9b4' :
+                        d.value > 50 ? '#edf8b1' :
+                          '#ffffcc';});
+    });
+
 }
